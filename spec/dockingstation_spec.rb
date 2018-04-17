2b54aaa3b9dde94242c07176f2c9b6625e3de911
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-17 16:14:45 +0100

    release bike unit test

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,2 +1,8 @@
+require 'dockingstation'
+
 describe DockingStation do
+
+  it 'expects instances of "DockingStation" to repond to "release_bike"' do
+    expect(DockingStation.new).to respond_to?.release_bike
+  end
 end
