ebe6059e908bb185e5d4e6e4bbeff3419c33854f
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-18 17:53:08 +0100

    End of Day3 with Zoe, Commit 1

---
+++ lib/dockingstation.rb
@@ -0,0 +1,13 @@
+class DockingStation
+attr_reader :bike
+def initialize
+@bike = bike
+end
+
+def dock_bike
+end
+
+def release_bike
+  Bike.new
+end
+end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,16 @@
 require 'dockingstation'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it 'releases a working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  respond_to
+
+
   end
+  it 'docks a bike' do
+    expect(dock_bike).to rese
+  end
+end
