8acb4197b291f13f180b0074d2205bfd5d253ecf
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-18 18:01:21 +0100

    End of Day 3, with Zoe - commit 2

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,14 +3,7 @@ require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it 'releases a working bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  respond_to
 
+  it { is_expected.to respond_to :dock_bike }
 
-  end
-  it 'docks a bike' do
-    expect(dock_bike).to rese
-  end
 end
