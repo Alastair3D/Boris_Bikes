9e47ad56bc3aaa5820c69395899136652facd5a9
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-19 14:01:37 +0100

    End of Step 12

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,11 +1,12 @@
 class DockingStation
   attr_reader :bike
 
-  def initialize
-    @bike = bike
-  end
+  # # def initialize
+  #   @bike = bike
+  # end
 
   def dock_bike(bike)
+    bike = @bike 
   end
 
   def release_bike
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -9,4 +9,8 @@ describe DockingStation do
   it 'Does not release a bike when no bike present' do
     expect { subject.release_bike }.to raise_error
   end
+
+  it 'Does not allow bike to be docked when docking station is full' do
+    expect { subject.dock_bike }.to raise error
+  end
 end
